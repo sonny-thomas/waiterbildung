c028566679fba63b3100239b15e9c3bfb9ba5dfb
sonny-thomas <sonnythomas0618@gmail.com>
2024-10-21 20:56:24 +0100

    feat: initialize fastapi and integrate celery

---
+++ celery_config.py
@@ -0,0 +1,17 @@
+import os
+from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
+
+celery_app = Celery(
+    __name__,
+    broker=os.getenv("CELERY_BROKER_URL"),
+    backend=os.getenv("CELERY_RESULT_BACKEND"),
+)
+
+celery_app.conf.update(
+    imports=["main"],
+    broker_connection_retry_on_startup=True,
+    task_track_started=True,
+)
