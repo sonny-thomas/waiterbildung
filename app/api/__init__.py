d854f79f07f1d5f1ed8fa9b3e99b319a24d8cf63
sonny-thomas <sonnythomas0618@gmail.com>
2025-02-17 13:20:30 +0100

    fix: reorder API router imports for consistency

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from app.api import auth, user, institution
+from app.api import auth, institution, user
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
-api_router.include_router(institution.router)
 api_router.include_router(user.router)
+api_router.include_router(institution.router)
